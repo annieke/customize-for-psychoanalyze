a37648fea5c656df871f635eb32e06f1a1db8c99
annie ke <annieke8@gmail.com>
2017-04-15 22:06:48 -0400

    answers change css, done with 3/5

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+$('input[type="radio"]').change(function() {
+  let checkedName = this.name;
+  $('input[type="radio"]').each((i, element) => {
+    if (element.name === checkedName) {
+      if (element.checked) {
+        $(element).parent().css({
+          opacity: "1",
+        });
+        $(element).parent().parent().addClass("buzzdiv");
+      } else {
+        $(element).parent().css({
+          opacity: "0.4",
+        });
+        $(element).parent().parent().removeClass("buzzdiv");
+      }
+    }
+  });
+});
+
 $('[name="header-image"]').change(function() {
   switch (this.value) {
     case "meme" :
@@ -16,7 +35,51 @@ $('[name="header-image"]').change(function() {
       $('#header')[0].style.backgroundImage = "url('img/header5.gif')";
       break;
     case "lazy" :
-      $('#header')[0].style.backgroundImage = "none"; // fix?? 
+      $('#header')[0].style.backgroundImage = "url('img/header6.png')";
+      break;
+    default:
+      break;
+  }
+});
+
+$('[name="font"]').change(function() {
+  switch (this.value) {
+    case "meme" :
+      $('body').css("font-family", 'Comic Sans MS');
+      break;
+    case "what" :
+      $('body').css("font-family", 'Hanalei');
+      break;
+    case "cliche" :
+      $('body').css("font-family", 'Amatica SC');
+      break;
+    case "rebel" :
+      $('body').css("font-family", 'UnifrakturCook');
+      break;
+    case "old" :
+      $('body').css("font-family", 'Italianno');
+      break;
+    case "lazy" :
+      $('body').css("font-family", 'Times');
+      break;
+    default:
+      break;
+  }
+});
+
+$('[name="text-align"]').change(function() {
+  switch (this.value) {
+    case "meme" :
+      $('*').css("text-align", "center");
+      $('#header').css("text-align", "center");
+      break;
+    case "what" :
+      $('*').css("text-align", "right");
+      $('#header').css("text-align", "right");
+      break;
+    case "old" :
+      $('*').css("text-align", "left");
+      $('#header').css("text-align", "left");
       break;
     default:
       break;
